dc24cd57de353f7d3319070ef2492596643ad7f3
brunogonzaga <babsgonzaga@gmail.com>
2021-10-23 00:38:04 -0300

    Terminada rotina padrão.

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, except: [:destroy]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if !params[:bookmark][:movie_id].empty?
+      @bookmark.movie = Movie.find(params[:bookmark][:movie_id])
+    end
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
